ff73b617c3231874e97a265c0f6ee99d8adb03ac
aleferamos <alefepdias@gmail.com>
2022-06-02 14:33:04 -0400

    t

--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -1,23 +1,14 @@
 public class TesteEmail {
 
     static boolean testar_email_com_arroba(){
-        if(Pessoa.emailValid("email_teste@dominio.com.br")){
-            return true;
-        }
-       return false;
+        return Pessoa.emailValid("email_teste@dominio.com.br");
     }
 
     static boolean testar_email_sem_arroba(){
-        if(!Pessoa.emailValid("email_testedominio.com.br")){
-            return true;
-        }
-        return false;
+        return !Pessoa.emailValid("email_testedominio.com.br");
     }
 
     static boolean testar_email_mais_50_caracteres(){
-        if(!Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br")){
-            return true;
-        }
-        return false;
+        return !Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
     }
 }
