3d5758fcc80624757a9f079344c0c82c0cf0efb8
aleferamos <alefepdias@gmail.com>
2022-06-02 14:36:48 -0400

    t

--- 0x00/calculadora/OperacaoBasicaTeste.java
+++ 0x00/calculadora/OperacaoBasicaTeste.java
@@ -1,34 +1,18 @@
 public class OperacaoBasicaTeste {
 
     static boolean testar_soma(){
-        if(OperacaoBasica.somar(5,3) == 8){
-            return true;
-        }
-
-        return false;
+        return OperacaoBasica.somar(5,3) == 8;
     }
 
     static boolean testar_subtracao(){
-        if(OperacaoBasica.subtrair(5,3) == 2){
-            return true;
-        }
-
-        return false;
+        return OperacaoBasica.subtrair(5,3) == 2;
     }
 
     static boolean testar_multiplicacao(){
-        if(OperacaoBasica.multiplicar(5,3) == 15){
-            return true;
-        }
-
-        return false;
+        return OperacaoBasica.multiplicar(5,3) == 15;
     }
 
     static boolean testar_divisao(){
-        if(OperacaoBasica.dividir(9,3) == 3){
-            return true;
-        }
-
-        return false;
+        return OperacaoBasica.dividir(9,3) == 3;
     }
 }
